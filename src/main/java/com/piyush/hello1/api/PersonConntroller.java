1ca817a72422162c303b45f958e618e8d4af810e
Piyush Diwakar <pidiwakar12@gmail.com>
2020-05-24 12:17:37 +0530

    first springboot

--- src/main/java/com/piyush/hello1/api/PersonConntroller.java
+++ src/main/java/com/piyush/hello1/api/PersonConntroller.java
@@ -3,9 +3,14 @@ package com.piyush.hello1.api;
 import com.piyush.hello1.model.Person;
 import com.piyush.hello1.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.List;
+import java.util.UUID;
+
+@RequestMapping("api/v1/person")
 @RestController
 public class PersonConntroller {
 
@@ -17,7 +22,48 @@ public class PersonConntroller {
     }
 
     @PostMapping
-    public void addPerson(Person person){
+    public void addPerson(@Valid @NonNull @RequestBody Person person){
         personService.addPerson(person);
     }
+
+    @GetMapping
+    List<Person> selectAllPeople() {
+        return personService.getAllPeople();
+    }
+
+    @GetMapping(path= "{id}")
+    public Person getPersonById(@PathVariable("id") UUID id){
+        return personService.getPersonById(id)
+                .orElse(  null);
+    }
+    @DeleteMapping(path = "{id}")
+    public void deletePersonById(@PathVariable("id") UUID id){
+        personService.deletePerson(id);
+    }
+
+    @PutMapping(path = "{id}")
+    public void updatePersonById(@PathVariable("id") UUID id,@Valid @NonNull @RequestBody Person personToUpdate){
+        personService.updatePerson(id,personToUpdate);
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/piyush/hello1/service/PersonService.java
+++ src/main/java/com/piyush/hello1/service/PersonService.java
@@ -6,13 +6,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
 public class PersonService {
 
     private final PersonDao personDao;
 
     @Autowired
-    public PersonService(@Qualifier("fakeDao") PersonDao personDao) {
+    public PersonService(@Qualifier("postgres") PersonDao personDao) {
         this.personDao = personDao;
     }
 
@@ -20,4 +24,32 @@ public class PersonService {
     return personDao.insertPerson(person);
 }
 
+    public List<Person> getAllPeople(){
+        return personDao.selectAllPeople();
+    }
+
+    public Optional<Person> getPersonById(UUID id){
+        return personDao.selectPersonById(id);
+    }
+
+    public int deletePerson(UUID id){
+        return personDao.deletePersonById(id);
+    }
+
+    public int updatePerson(UUID id, Person newPerson){
+        return personDao.updatePersonById(id,newPerson);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
